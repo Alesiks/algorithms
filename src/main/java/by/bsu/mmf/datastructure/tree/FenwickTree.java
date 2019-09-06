625bc319f4f6f9768db85d672036a69c4478e5e6
Ales Zhuk <ales.zhook@gmail.com>
2019-09-06 23:48:54 +0300

    Fenwick tree fixed

--- src/main/java/by/bsu/mmf/datastructure/tree/FenwickTree.java
+++ src/main/java/by/bsu/mmf/datastructure/tree/FenwickTree.java
@@ -1,38 +1,39 @@
 package by.bsu.mmf.datastructure.tree;
 
 // allows to calculate the value of some reversible operation F on any segment [L; R] in logarithmic time;
-// allows you to change the value of any element in O(nlogn)
+// allows you to change the value of any element in O(logn)
 // implementation for calculating sum on interval
-public class FenwickTree {
+class FenwickTree {
 
-    private int treeArr[];
+    private long[] arr;
 
-    public FenwickTree(int[] values) {
-        treeArr = new int[values.length + 1];
-        for(int i = 0; i < values.length; i++) {
-            update(i + 1, values[i]);
-        }
-
-    }
-
-    public int sum(int range) {
-        int i = range;
-        int res = 0;
-        while (i >= 0) {
-            res += treeArr[i];
-            i -= (i & -i);
-        }
-        return res;
+    public FenwickTree(int size) {
+        this.arr = new long[size + 1];
     }
 
     public void update(int i, int delta) {
-        while (i < treeArr.length) {
-            treeArr[i] += delta;
-            i += (i & -i);
+        while (i < arr.length) {
+            arr[i] += delta;
+            i += getLeastSignificantNonZeroBit(i);
         }
     }
 
-    public int[] getTreeArr() {
-        return treeArr;
+    public long sumRange(int range) {
+        long sum = 0;
+        while (range > 0) {
+            sum += arr[range];
+            range -= getLeastSignificantNonZeroBit(range);
+        }
+        return sum;
     }
+
+    public long sumInterval(int a, int b) {
+        return sumRange(b) - sumRange(a - 1);
+    }
+
+    private int getLeastSignificantNonZeroBit(int num) {
+        return num & -num;
+    }
+
 }
+
--- src/test/java/by/bsu/mmf/datastructure/tree/FenwickTreeTest.java
+++ src/test/java/by/bsu/mmf/datastructure/tree/FenwickTreeTest.java
@@ -4,16 +4,22 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(JUnit4.class)
 public class FenwickTreeTest {
 
     @Test
     public void sumTest() {
         int[] values = {1, 2, 1, 2, 4, 5, 7, -1, 9};
-        FenwickTree fenwickTree = new FenwickTree(values);
-        int treeArr[] = fenwickTree.getTreeArr();
-        for (int i = 0; i < treeArr.length; i++) {
-            System.out.print(treeArr[i] + " ");
+        FenwickTree fenwickTree = new FenwickTree(values.length);
+        for (int i = 0; i < values.length; i++) {
+            fenwickTree.update(i + 1, values[i]);
+        }
+
+        int[] expectedSumArray = new int[]{1,3,4,6,10,15,22,21,30};
+        for (int i = 0; i < values.length; i++) {
+            assertEquals(fenwickTree.sumRange(i + 1), expectedSumArray[i]);
         }
     }
 
